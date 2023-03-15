82183df6f6115400c4ec33d9a4ead873d27231af
aakankshsingh02 <aakankshsingh012@gmail.com>
2023-03-15 22:46:33 +0530

    commit

---
+++ pattern/src/pattern10.java
@@ -0,0 +1,33 @@
+/*
+This is the 10th pattern 
+ * 
+ **
+ ***
+ ****
+ *****
+ ****
+ ***
+ **
+ *
+ 
+*/
+
+import java.io.*;
+import java.util.*;
+class pattern10
+{
+   public static void main(String [] args)throws IOException {
+    
+    Scanner sc = new Scanner(System.in);
+    int n= sc.nextInt();
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.println("*");
+
+        }
+        System.out.println("");
+    }
+   } 
+}
