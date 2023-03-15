0ae9bec0180b68071d32eddbcc8c0fc343c85584
aakankshsingh02 <aakankshsingh012@gmail.com>
2023-03-15 23:12:15 +0530

    commit

---
+++ pattern/src/pattern15.java
@@ -0,0 +1,38 @@
+/* 
+This is the 13th Pattern
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
+
+import java.util.*;
+ class pattern15 {
+    public static void main(String[] args) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        
+        
+        for(int i=n;i>=1;i--)
+        {
+            for(char j='A';j<'A'+i;j++)
+            {
+                System.out.print(j);
+                
+            }
+                
+               
+                
+            System.out.println("");
+           
+        }
+        
+       
+
+            
+        
+    }
+    
+}
